a7adfa1795b192dc9f9523ed533614cfbbc025fd
Roman Nikolaenkov <romanliveshere@gmail.com>
2025-12-09 19:41:22 -0800

    kotlin leap year

---
+++ kotlin/leap/src/main/kotlin/Leap.kt
@@ -0,0 +1,9 @@
+data class Year(val year : Int) {
+
+    val isLeap: Boolean = when {
+        year % 400 == 0 -> true
+        year % 100 == 0 -> false
+        year % 4 == 0 -> true
+        else -> false
+    }
+}
