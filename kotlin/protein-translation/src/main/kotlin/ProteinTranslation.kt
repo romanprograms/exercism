d85dabef6f643c2f4fbfe859efdbb13d3fac68f6
Roman Nikolaenkov <romanliveshere@gmail.com>
2025-12-14 18:53:03 -0800

    kotlin protein-translation improved solution

--- kotlin/protein-translation/src/main/kotlin/ProteinTranslation.kt
+++ kotlin/protein-translation/src/main/kotlin/ProteinTranslation.kt
@@ -1,38 +1,24 @@
-val codonAminoAcidMap = mapOf(
-    "AUG" to "Methionine", "UUU" to "Phenylalanine",
-    "UUC" to "Phenylalanine", "UUA" to "Leucine", "UUG" to "Leucine", "UCU" to "Serine",
-    "UCC" to "Serine", "UCA" to "Serine",
-    "UCG" to "Serine", "UAU" to "Tyrosine", "UAC" to "Tyrosine", "UGU" to "Cysteine",
-    "UGC" to "Cysteine", "UGG" to "Tryptophan", "UAA" to "STOP", "UAG" to "STOP", "UGA" to "STOP"
-)
-
 fun translate(rna: String?): List<String> {
-    var codons = mutableListOf<String>()
+    if (rna.isNullOrEmpty()) {
+        return emptyList()
+    }
 
-    var tmpCodon = StringBuilder()
+    val result: MutableList<String> = mutableListOf()
 
-    if (rna != null) {
-        for (char in rna) {
-            if (tmpCodon.length == 3) {
-                if (!codonAminoAcidMap.containsKey(tmpCodon.toString())) {
-                    throw IllegalArgumentException("Invalid codon")
-                }
-                if (codonAminoAcidMap.getValue(tmpCodon.toString()) == "STOP") {
-                    tmpCodon = StringBuilder()
-                    break;
-                }
-                codons.add(tmpCodon.toString())
-                tmpCodon = StringBuilder()
-            }
-            tmpCodon.append(char)
-        }
-        if ( tmpCodon.isNotBlank() && !codonAminoAcidMap.containsKey(tmpCodon.toString())) {
-            throw IllegalArgumentException("Invalid codon")
-        }
-        if (tmpCodon.isNotBlank() && codonAminoAcidMap.getValue(tmpCodon.toString()) != "STOP") {
-            codons.add(tmpCodon.toString())
+    var codons = rna.chunked(3).toList()
+    for (codon in codons) {
+        when (codon) {
+            "AUG" -> result.add("Methionine")
+            "UUU", "UUC" -> result.add("Phenylalanine")
+            "UUA", "UUG" -> result.add("Leucine")
+            "UCU", "UCC", "UCA", "UCG" -> result.add("Serine");
+            "UAU", "UAC" -> result.add("Tyrosine")
+            "UGU", "UGC" -> result.add("Cysteine")
+            "UGG" -> result.add("Tryptophan")
+            "UAA", "UAG", "UGA" -> break
+            else -> throw IllegalArgumentException(codon)
         }
     }
-    val proteins = codons.map{codon -> codonAminoAcidMap.getValue(codon)}
-    return proteins
+
+    return result
 }
